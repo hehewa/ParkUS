0b432f296f32e84cfed5017c080eb4944f445616
Felix Martel-Denis <lixtelnis@gmail.com>
2017-04-10 15:10:52 -0400

    Page de fausses donnees

--- scripts/views/StatsAppView.jsx
+++ scripts/views/StatsAppView.jsx
@@ -6,28 +6,71 @@ class AppView extends React.Component {
     super();
     window.googleChartReactPackages = ['corechart'];
   }
-  drawPieChart(chartID) {
-    var data = new window.google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Mushrooms', 3],
-      ['Onions', 1],
-      ['Olives', 1],
-      ['Zucchini', 1],
-      ['Pepperoni', 2]
+  drawBarChart(chartID) {
+    var data = google.visualization.arrayToDataTable([
+      ['Heure', "Taux d'occupation", "Taux d'occupation moyen"],
+      ['04 h', 5, 10],
+      ['05 h', 8, 20],
+      ['06 h', 8, 30],
+      ['07 h', 30, 40],
+      ['08 h', 50, 50],
+      ['09 h', 80, 70],
+      ['10 h', 80, 70],
+      ['11 h', 80, 91],
+      ['12 h', 80, 60],
+      ['13 h', 90, 60],
+      ['14 h', 90, 60],
+      ['15 h', 70, 70],
+      ['16 h', 70, 70],
+      ['17 h', 60, 50],
+      ['18 h', 60, 40],
+      ['19 h', 60, 20],
+      ['20 h', 60, 10],
+      ['21 h', 50, 5]
     ]);
     var options = {
-      'title':'How Much Pizza I Ate Last Night',
-      'width':400,
-      'height':300
+      title: "Taux d'occupation horaire",
+      colors: ['#b0120a', '#ffab91'],
+      hAxis: {
+        title: "Taux d'occupation",
+        minValue: 0,
+        maxValue: 100
+      },
+      vAxis: {
+        title: 'Heure'
+      },
+      width: 900,
+      height: 500
+    };
+    var chart = new google.visualization.BarChart(document.getElementById(chartID));
+    chart.draw(data, options);
+  }
+  drawPieChart(chartID) {
+    var data = new window.google.visualization.DataTable();
+    data.addColumn('string', 'Type');
+    data.addColumn('number', 'Compte');
+    data.addRows([
+      ['Journalier', 5],
+      ['Annuel', 12],
+      ['Mensuel', 2]
+    ]);
+    var options = {
+      'title':"Type d'abonnement",
+      'width': 400,
+      'height': 300
     };
     var chart = new window.google.visualization.PieChart(document.getElementById(chartID));
     chart.draw(data, options);
   }
   
   render() {
-    return (<GoogleChart drawChart={this.drawPieChart} />);
+    return (
+      <span>
+        <GoogleChart drawChart={this.drawBarChart} />
+        <br/>
+        <GoogleChart drawChart={this.drawPieChart} />
+      </span>
+    );
   }
 }
 
